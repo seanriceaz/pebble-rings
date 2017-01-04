77b92edc12ea4026541634b203192d48df351b6e
Sean Rice <seanriceaz@users.noreply.github.com>
2017-01-04 08:16:59 -0700

    Adding some more code from the tutorial

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,5 +1,30 @@
+//This file is started from this: https://developer.pebble.com/tutorials/js-watchface-tutorial/part1/
 //Include our time library
 var rocky = require('rocky');
+
+function fractionToRadian(fraction) {
+  return fraction * 2 * Math.PI;
+}
+
+function drawAngle(ctx, cx, cy, angle, length, color){
+  // Find the end points
+  var x2 = cx + Math.sin(angle) * length;
+  var y2 = cy - Math.cos(angle) * length;
+
+  // Configure how we want to draw the hand
+  ctx.lineWidth = 8;
+  ctx.strokeStyle = color;
+
+  // Begin drawing
+  ctx.beginPath();
+
+  // Move to the center point, then draw the line
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(x2, y2);
+
+  // Stroke the line (output to display)
+  ctx.stroke();
+}
 
 //Every minute we update
 rocky.on('minutechange', function(event) {
@@ -9,10 +34,39 @@ rocky.on('minutechange', function(event) {
 rocky.on('draw', function(event) {
   // Get the CanvasRenderingContext2D object
   var ctx = event.context;
+
+  var d = new Date();
+
+  // Clear the screen
+  //ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+
+  // Determine the width and height of the display
+  var w = ctx.canvas.unobstructedWidth;
+  var h = ctx.canvas.unobstructedHeight;
+
+  // Determine the center point of the display
+  // and the max size of watch hands
+  var cx = w / 2;
+  var cy = h / 2;
+
+  // -20 so we're inset 10px on each side
+  var maxLength = (Math.min(w, h) - 20) / 2;
+
+  // Calculate the minute hand angle
+  var minuteFraction = (d.getMinutes()) / 60;
+  var minuteAngle = fractionToRadian(minuteFraction);
+
+  // Draw the minute hand
+  drawHand(ctx, cx, cy, minuteAngle, maxLength, "white");
+
+  // Calculate the hour hand angle
+  var hourFraction = (d.getHours() % 12 + minuteFraction) / 12;
+  var hourAngle = fractionToRadian(hourFraction);
+
+  // Draw the hour hand
+  drawHand(ctx, cx, cy, hourAngle, maxLength * 0.6, "lightblue");
+});
   
   //radius of the circle is based on the hour (smaller as hours is higher)
   
   //Angle drawn is based on the minute (minute 60 is a complete circle)
-  
-  
-});
